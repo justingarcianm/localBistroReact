f30ef9b50b58a8812d75e1eb2d125200ebcb61fa
justin garcia <justinframedev@gmail.com>
2019-02-28 08:36:35 -0700

    import css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,25 +5,25 @@ class Nav extends React.Component {
 	render() {
 		return (
 			<div id="home">
-				<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-					<a class="navbar-brand" href="#home"><i class="fas fa-utensils"></i> Local Bistro</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+				<nav className="navbar fixed-top navbar-expand-lg navbar-dark">
+					<a className="navbar-brand" href="#home"><i className="fas fa-utensils"></i> Local Bistro</a>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
 						aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-						Menu <i class="fas fa-caret-down"></i>
+						Menu <i className="fas fa-caret-down"></i>
 					</button>
-					<div class="collapse navbar-collapse" id="navbarNavDropdown">
-						<ul class="navbar-nav mx-auto">
-							<li class="nav-item active">
-								<a class="nav-link" href="#about">About <span class="sr-only">(current)</span></a>
+					<div className="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul className="navbar-nav mx-auto">
+							<li className="nav-item active">
+								<a className="nav-link" href="#about">About <span className="sr-only">(current)</span></a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#food">Food</a>
+							<li className="nav-item">
+								<a className="nav-link" href="#food">Food</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#drinks">Drinks</a>
+							<li className="nav-item">
+								<a className="nav-link" href="#drinks">Drinks</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#contact">Find Us</a>
+							<li className="nav-item">
+								<a className="nav-link" href="#contact">Find Us</a>
 							</li>
 						</ul>
 					</div>
