b81239e4e79111780436c6fc9c419b91f2188e5e
justin garcia <justinframedev@gmail.com>
2019-03-14 17:23:15 -0600

    add drink menu text

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -6,73 +6,53 @@ class Drinks extends React.Component {
 			<div className="text-center container-fluid menu">
 				<div className="menu-bottom">
 					<div className="row">
-						<div className="col-md-6 col-sm-12">
-							<h3>Wine</h3>
-							<div className="row">
-								<div className="col-md-4 col-sm-12">
-									<h4>White</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-
-								</div>
-								<div className="col-md-4 col-sm-12">
-									<h4>Red</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-								</div>
-								<div className="col-md-4 col-sm-12">
-									<h4>Dessert</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div className="col-md-6 col-sm-12">
-							<h3>Beer</h3>
-							<div className="row">
-								<div className="col-md-4 col-sm-12">
-									<h4>Domestic</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-								</div>
-								<div className="col-md-4 col-sm-12">
-									<h4>Local</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-								</div>
-								<div className="col-md-4 col-sm-12">
-									<h4>Imported</h4>
-									<ul>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-										<li>name - price/price</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-sm-12">
-							<h3>Coffee</h3>
+						<div className="col-md-4 col-sm-12">
+							<h2>wine <span>glass/bottle</span></h2>
+							<h3>white</h3>
 							<ul>
-								<li>name - price</li>
-								<li>name - price</li>
-								<li>name - price</li>
+								<li> Chardonnay, California - 10 /40 </li>
+								<li> Pino Grigio, Oregon - 9 /36  </li>
+								<li>  Sauvignon Blanc, New Zeland - 11 /40  </li>
+							</ul>
+							<h3>red</h3>
+							<ul>
+								<li>Merlot, Italy - 10 / 40 </li>
+								<li> Malbec, Argentina - 8 / 50 </li>
+								<li> Cotes de Rhone Rouge, France - 15 / 50 </li>
+							</ul>
+							<h3>dessert</h3>
+							<ul>
+								<li> Rose - 12 / 40 </li>
+								<li> Port - 15 / 60 </li>
+							</ul>
+						</div>
+						<div className="col-md-4 col-sm-12">
+							<h2>beer <span>12oz/24oz</span></h2>
+							<h3>domestic</h3>
+							<ul>
+								<li> Budwiser - 3 / 5 </li>
+								<li> Coors Light - 4 / 5 </li>
+							</ul>
+							<h3>imported</h3>
+							<ul>
+								<li>Stella Artois - 4.75 / 6 </li>
+								<li> Dos Equis Lager - 4.75 / 6 </li>
+								<li> Guinness - 6.5 / 7 </li>
+							</ul>
+							<h3>local</h3>
+							<ul>
+								<li> La Cumbre Elevated IPA - 5.5 /7</li>
+								<li> Marble Double White - 6 /8</li>
+							</ul>
+						</div>
+						<div className="col-md-4 col-sm-12">
+							<h2>coffee</h2>
+							<ul>
+								<li>brew - 2 </li>
+								<li> espresso - 1.5 </li>
+								<li>cappuccino - 4.5 </li>
+								<li>Latte - 4.5 </li>
+								<li> Flavored Latte - 5.5 </li>
 							</ul>
 						</div>
 					</div>
