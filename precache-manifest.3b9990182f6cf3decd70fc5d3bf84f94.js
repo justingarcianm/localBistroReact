4a59f8562492042a4fd58ac964ed506c120d18de
justin garcia <justinframedev@gmail.com>
2019-03-18 15:08:44 -0600

    Updates

---
+++ precache-manifest.3b9990182f6cf3decd70fc5d3bf84f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a066ccaaa8690e8c8a",
+    "url": "/localBistroReact/static/js/runtime~main.12a066cc.js"
+  },
+  {
+    "revision": "2bd9096bbc093f67e251",
+    "url": "/localBistroReact/static/js/main.2bd9096b.chunk.js"
+  },
+  {
+    "revision": "536adba0ce4b26418e22",
+    "url": "/localBistroReact/static/js/2.536adba0.chunk.js"
+  },
+  {
+    "revision": "2bd9096bbc093f67e251",
+    "url": "/localBistroReact/static/css/main.852b2c14.chunk.css"
+  },
+  {
+    "revision": "536adba0ce4b26418e22",
+    "url": "/localBistroReact/static/css/2.c01a1190.chunk.css"
+  },
+  {
+    "revision": "4315e4c89ded43d471c420d118e72d0e",
+    "url": "/localBistroReact/index.html"
+  }
+];
