99faaf72f1294dd5d4d286fec4f09577ee2f2e44
justin garcia <justinframedev@gmail.com>
2019-03-26 19:24:44 -0600

    fill in form to run form function

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -3,30 +3,44 @@ import Calendar from './Calender'
 class Reservations extends React.Component {
 	constructor() {
 		super()
-		this.state = {}
+		this.state = {
+			name: "",
+			email: "",
+			date: "",
+			time: "",
+			guests: ""
+		}
+		this.formFunction = this.formFunction.bind(this)
+	}
+
+	formFunction(event) {
+		event.preventDefault()
+		console.log("submit")
 	}
 
 	render() {
+
+
 		return (
 			<div>
 				<h2 className="text-center">Reserve Today!</h2>
-				<form>
+				<form onSubmit={this.formFunction}>
 					<div className="form-group">
 						<label>Who is the reservation for?</label>
-						<input type="text" placeholder="name" class="form-control" />
+						<input name="name" value={this.state.name} type="text" placeholder="name" className="form-control" />
 					</div>
 					<div className="form-group">
 						<label>Where should the confirmation be sent?</label>
-						<input type="email" placeholder="email" class="form-control" />
+						<input name="email" value={this.state.email} type="email" placeholder="email" className="form-control" />
 					</div>
-					<div class="form-group">
+					<div className="form-group">
 						<label>What day and time?</label>
 						<div className="row">
 							<div className="col-sm-4">
-								<Calendar />
+								<Calendar name="date" value={this.state.date} />
 							</div>
 							<div className="col-sm-8">
-								<select class="form-control">
+								<select className="form-control" name="time" value={this.state.time}>
 									<option>--Select a Time--</option>
 									<option>8:00 AM</option>
 									<option>9:00 AM</option>
@@ -47,7 +61,7 @@ class Reservations extends React.Component {
 							</div>
 						</div>
 						<label>How many guests will be joining?</label>
-						<select class="form-control">
+						<select className="form-control">
 							<option>--Select number of guests--</option>
 							<option>1</option>
 							<option>2</option>
@@ -60,7 +74,7 @@ class Reservations extends React.Component {
 
 
 
-					<button className="btn form-button" disabled>Submit</button>
+					<button className="btn form-button">Submit</button>
 				</form>
 			</div>
 		)
